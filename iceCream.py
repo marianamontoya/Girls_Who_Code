fc0c44629c14e80aaab97161bef31b07333a1812
marianamontoya <marianamontoya1120@gmail.com>
2017-06-29 10:08:03 -0700

    Add files via upload

---
+++ iceCream.py
@@ -0,0 +1,48 @@
+print("Welcome to the Jariana Ice Cream Parlor!")
+print("What's your name?")
+name = input()
+if (name == "DIANA" or name =="Dia" or name =="diana" or name =="Anah" or name =="Jessica" or name =="Diana"):
+    print ("We don't serve people who break out codes!")
+    exit()
+    
+print("How hungry are you %s?" %(name))
+print("I'm Hungry, Starving, I'm boutta faint!!")
+
+
+choice = input()   
+
+if (choice == "Hungry" or "I'm Hungry" or "im hungry"):
+    print ("So just one scoop?")   
+elif (choice == "Starving" or "I'm starving" or "im starving"):
+    print ("So just two Scoops?")
+elif (choice == "I'm boutta faint" or "faint"):
+    print ("Danggg you want three scoops!")
+elif ("I'm not hungry"):
+    exit: ()
+
+print("We got Madagascar Vanilla, Rainbow Sherbert, and Drizzling Cookie Dough")
+
+choice = input ()
+
+if (choice == "Madagascar Vanilla"):
+    print("You're plain as H, E double hockey sticks!")
+elif (choice == "Rainbow Sherbert"):
+    print("You sassy girl!")
+elif (choice == "Drizzling Cookie Dough"):
+    print("You're a loner")
+else:
+    print("We don't got that here, what do you think this is Baskin Robins!")
+
+        
+print("Cup or cone?")
+
+choice = input ()
+
+if (choice == "Cup"):
+    print("Good choice, you're not a messy eater!")
+if (choice == "Cone"):
+    print("Be sure to grab a napkin on your way out!!")
+
+print("Here's your mediocore ice cream for a mediocore person.")
+exit:()
+
